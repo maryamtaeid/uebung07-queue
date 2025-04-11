e1377fac5a5f871a6a4b69bdb9d3fad9230ad2a7
maryam <maryam.taeid@edu.fh-joanneum.at>
2025-04-11 18:26:04 +0200

    Add JavaDoc comments for code documentation

--- src/main/java/at/fhj/msd/Drink.java
+++ src/main/java/at/fhj/msd/Drink.java
@@ -1,46 +1,60 @@
 package at.fhj.msd;
 
 /**
- * Abstract base class for concrete drink classes
+ * Abstract base class for concrete drink classes.
+ * This class provides common functionality for all drinks.
  */
 public abstract class Drink {
+    
     /**
-     * name of the drink
+     * Name of the drink.
      */
     protected String name;
+
     /**
-     * Creates a Drink object with given name
+     * Creates a Drink object with the given name.
      *
-     * @param name name of the drink
-     *
+     * @param name the name of the drink
      */
     public Drink(String name) {
         this.name = name;
     }
+
     /**
-     * Calculates and returns volume of drink
+     * Calculates and returns the volume of the drink.
      *
-     * @return the volume of drink in liter
+     * @return the volume of the drink in liters
      */
     public abstract double getVolume();
+
     /**
-     * Calculates and returns the alcohol percentage
+     * Calculates and returns the alcohol percentage of the drink.
      *
-     * @return alcohol volume percent (e.g. 50)
+     * @return the alcohol volume percentage (e.g., 50 for 50%)
      */
     public abstract double getAlcoholPercent();
+
     /**
-     * Gives information if drink is alcoholic or not
+     * Determines if the drink is alcoholic or not.
      *
-     * @return true when alcoholic liquids are present, otherwise false
+     * @return true if the drink is alcoholic, false otherwise
      */
     public abstract boolean isAlcoholic();
 
-
+    /**
+     * Gets the name of the drink.
+     *
+     * @return the name of the drink
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the drink.
+     *
+     * @param name the new name of the drink
+     */
     public void setName(String name) {
         this.name = name;
     }
